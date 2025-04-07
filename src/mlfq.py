10bffee23d46efbc848aae1133c3499a77084429
JabelResendiz <jabelresendiz18@gmail.com>
2025-04-07 17:12:23 -0400

    update mlfq

--- src/mlfq.py
+++ src/mlfq.py
@@ -1,97 +1,3 @@
-
-# import heapq
-# from collections import deque
-# from basic_struct import Process,Event
-# from scheduling import Scheduling
-
-
-# class MLFQScheduler(Scheduling):
-#     def  __init__(self,quantums):
-#         super().__init__()
-#         self.quantums = quantums
-#         self.queues = [deque() for _ in quantums]+ [deque()]
-#         self.running_process = None
-#         self.process_levels = {}
-
-#     def add_process(self, process: Process):
-#         self.processes.append(process)
-#         self.process_levels[process.pid] = 0
-#         arrival_event = Event(process.arrival_time,'ARRIVAL',process)
-#         heapq.heappush(self.event_queue,arrival_event)
-
-#     def run(self):
-#         while self.event_queue:
-#             event = heapq.heappop(self.event_queue)
-#             self.current_time = event.time
-#             self.process_event(event)
-    
-#     def process_event(self, event: Event):
-#         if event.event_type == 'ARRIVAL':
-#             self.handle_arrival(event.process)
-#         elif event.event_type == 'TIME_SLICE_EXPIRED':
-#             self.handle_time_slice_expired(event.process)
-#         elif event.event_type == 'COMPLETION':
-#             self.handle_completion(event.process)
-    
-
-#     def handle_arrival(self, process: Process):
-#         self.queues[0].append(process)
-#         self.try_schedule_next()
-    
-#     def handle_time_slice_expired(self, process: Process):
-#         self.running_process = None
-#         level = self.process_event[process.pid]
-
-#         if level + 1 < len(self.queues):
-#             self.process_levels[process.pid] += 1
-        
-#         self.queues[self.process_levels[process.pid]] . append(process)
-
-#         self.try_schedule_next()
-    
-
-#     def handle_completion(self, process: Process):
-#         self.running_process = None
-#         process.end_time = self.current_time
-#         process.turnaround_time = process.end_time - process.arrival_time
-#         process.waiting_time = process.turnaround_time- process.burst_time
-#         self.completed_processes.append(process)
-#         self.try_schedule_next()
-    
-#     def try_schedule_next(self):
-#         if self.running_process:
-#             return
-
-#         for level,queue in enumerate(self.queues):
-#             if queue:
-#                 process = queue.popleft()
-#                 self.running_process = process
-
-#                 if process.start_time is None:
-#                     process.start_time = self.current_time
-                
-#                 if level < len(self.quantums):
-#                     quantum = self.quantums[level]
-#                     time_slice = min (quantum, process.remaining_time)
-                
-#                 else:
-#                     time_slice = process.remaining_time
-                
-#                 process.remaining_time -= time_slice
-
-#                 if process.remaining_time == 0:
-#                     event_type = 'COMPLETION'
-                
-#                 else:
-#                     event_type = 'TIME_SLICE_EXPIRATION'
-                
-#                 heapq.heappush(self.event_queue, Event(self.current_time+ time_slice,event_type,process))
-
-#                 break
-
-
-
-
 import heapq
 from collections import deque
 from basic_struct import Process, Event
@@ -100,14 +6,14 @@ from scheduling import Scheduling
 class MLFQScheduler(Scheduling):
     def __init__(self, quantums):
         super().__init__()
-        self.quantums = quantums  # e.g. [4, 8]
-        self.queues = [deque() for _ in quantums] + [deque()]  # última cola sin quantum = FCFS
+        self.quantums = quantums  
+        self.queues = [deque() for _ in quantums] + [deque()]  
         self.running_process = None
-        self.process_levels = {}  # PID -> current level
+        self.process_levels = {}  
 
     def add_process(self, process: Process):
         self.processes.append(process)
-        self.process_levels[process.pid] = 0  # comienza en el nivel más alto
+        self.process_levels[process.pid] = 0 
         arrival_event = Event(process.arrival_time, 'ARRIVAL', process)
         heapq.heappush(self.event_queue, arrival_event)
 
@@ -133,7 +39,7 @@ class MLFQScheduler(Scheduling):
         self.running_process = None
         level = self.process_levels[process.pid]
         if level + 1 < len(self.queues):
-            self.process_levels[process.pid] += 1  # degrada el proceso
+            self.process_levels[process.pid] += 1  
         self.queues[self.process_levels[process.pid]].append(process)
         self.try_schedule_next()
 
@@ -147,7 +53,7 @@ class MLFQScheduler(Scheduling):
 
     def try_schedule_next(self):
         if self.running_process:
-            return  # ya hay uno corriendo
+            return 
 
         for level, queue in enumerate(self.queues):
             if queue:
@@ -156,10 +62,10 @@ class MLFQScheduler(Scheduling):
                 if process.start_time is None:
                     process.start_time = self.current_time
 
-                if level < len(self.quantums):  # RR
+                if level < len(self.quantums): 
                     quantum = self.quantums[level]
                     time_slice = min(quantum, process.remaining_time)
-                else:  # FCFS
+                else:  
                     time_slice = process.remaining_time
 
                 process.remaining_time -= time_slice
